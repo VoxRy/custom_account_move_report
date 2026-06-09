374d2ea027479a29a8969573e871aa96d409ec19
Kais Akram <kakram@sevengates.co>
2026-06-09 09:38:05 +0300

    x

---
+++ custom_account_move_report/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Custom Account Move Report',
+    'version': '15.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Muhasebe Fişi Özel Rapor Tasarımı',
+    'depends': ['account', 'account_ux'],
+    'data': [
+        'report/report_account_move_document.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
